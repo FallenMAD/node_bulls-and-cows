3949afd28a42594c53bd5c9048919aebe0805eab
FallenMAD <naruto.yzymaki4@gmail.com>
2025-04-04 15:58:35 +0200

    bug fix second try

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const terminal = readline.createInterface(process.stdin, process.stdout);
 const randomNumber = generateRandomNumber();
 
 function startGame() {
-  terminal.write('Game started\n')
+  terminal.write('Game started\n');
 
   terminal.question('Please write your number - ', (name) => {
     if (!checkIsValidUserInput(name)) {
@@ -19,7 +19,7 @@ function startGame() {
 
       return;
     }
-    console.log(randomNumber);
+
     const result = getBullsAndCows(name, randomNumber);
 
     terminal.write(`Result: bulls ${result.bulls} & cows ${result.cows}\n`);
